b357350ce3b6b05a49d1475a28ca969d21348fa6
Jan Mussler <jan.mussler@zalando.de>
2015-12-02 13:50:24 +0100

    Make sleep time configurable

--- run.sh
+++ run.sh
@@ -2,10 +2,14 @@
 
 cd /
 
+if [ -z $AGENT_SLEEP_SECONDS ] ; then
+  export AGENT_SLEEP_SECONDS=60
+fi
+
 while :
 do
     echo "Executing agent..."
     python -m zmon_agent -e $(cat /etc/entity_service_url)
-    echo "sleeping..."
-    sleep 60
+    echo "sleeping..." $AGENT_SLEEP_SECONDS "seconds"
+    sleep $AGENT_SLEEP_SECONDS
 done
